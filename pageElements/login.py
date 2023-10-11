f0b744b999902e59fcbfe44cf9f9338bf0955029
rejan.bajracharya <rejan.bajracharya@cotiviti.com>
2023-10-11 13:17:40 +0545

    add type

--- pageElements/login.py
+++ pageElements/login.py
@@ -1,22 +1,35 @@
+from typing import List, Tuple
+
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Login:
-    username = (By.NAME, 'username')
-    password = (By.NAME, 'password')
-    login_btn = (By.NAME, 'signon')
-    login_link_text = (By.LINK_TEXT, 'Sign In')
-    logout_link_text = (By.LINK_TEXT, 'Sign Out')
+    username = (By.NAME, "username")
+    password = (By.NAME, "password")
+    login_btn = (By.NAME, "signon")
+    login_link_text = (By.LINK_TEXT, "Sign In")
+    logout_link_text = (By.LINK_TEXT, "Sign Out")
+    errors = (By.XPATH, "//div[@id='Catalog']//*[contains(text(), 'Please enter your username and password.')]")
+    invalid_creds = (By.XPATH, "//ul[@class='messages']//*[contains(text(), 'Signon failed')]")
 
-    def __init__(self, driver, wait):
+    def __init__(self, driver: WebDriver, wait: WebDriverWait):
         self.driver = driver
         self.wait = wait
+
+    def get_errors(self) -> List[WebElement]:
+        return self.driver.find_elements(*self.errors)
+
+    def get_invalid_creds(self):
+        return self.driver.find_elements(*self.invalid_creds)
 
     def get_login_elements(self):
         return self.username, self.password
 
-    def set_input_text(self, locator, value):
+    def set_input_text(self, locator: Tuple[str, str], value: str):
         el = self.wait.until(EC.visibility_of_element_located(locator))
         el.clear()
         el.send_keys(value)
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,24 +1,43 @@
+from  typing import List
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
+from testCases.base import BaseTest
 from pageElements.login import Login
+from selenium.webdriver.remote.webelement import WebElement
 
 
-class TestLogin:
-    username = '1001'
+class TestLogin(BaseTest):
+    username = 'supertest'
     password = 'supertest'
     base_url = 'https://petstore.octoperf.com/actions/Catalog.action'
 
     def test_login(self, setup):
         driver, wait = setup
-
         lp = Login(driver, wait)
         username_el, password_el = lp.get_login_elements()
-        print(username_el, password_el)
 
         # get login page
         lp.login_link()
         lp.set_input_text(username_el, self.username)
         lp.set_input_text(password_el, self.password)
         lp.login()
+
+        invalid_creds: List[WebElement] = lp.get_invalid_creds()
+        print(invalid_creds)
+        if invalid_creds:
+            for error in invalid_creds:
+                print(error.text)
+            return
+
+        errors: List[WebElement] = lp.get_errors()
+        if errors:
+            for error in errors:
+                print(error.text)
+            return
+
+        print('Login success')
+
+
